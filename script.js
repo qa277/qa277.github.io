12786c64295a47bf763aeee484157a48e301b178
qa277 <90406930+qa277@users.noreply.github.com>
2022-02-28 13:51:39 -0800

    Update script.js
    yes

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 let board = []
 const boardElements = document.querySelectorAll(".grid-square")
 let turn = 0;
@@ -10,14 +6,15 @@ for (let i = 0; i < 9; i++) {
     board.push({sym: i, el: boardElements[i], clicked: false});
     //boardElements[i].innerHTML = i;
     boardElements[i].onclick = function() {
-        if (board[i].cliked) {
-            board[i].clicked = false;
+        if (board[i].clicked == false) {
+            console.log(board[i].clicked)
             boardElements[i].style.backgroundColor = "lightblue";
             if (turn % 2 == 1) {
                 boardElements[i].innerHTML = "O";
             } else {
                 boardElements[i].innerHTML = "X";
             }
+            board[i].clicked = true;
             turn++;
         }
     }
